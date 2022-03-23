4d022713337c63b8dd9c0de0412d563b49ade24e
drew <andrewkemper1@gmail.com>
2022-03-23 16:58:23 -0400

    portfolio app start

---
+++ PortfolioApp/PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by Andrew Kemper on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
